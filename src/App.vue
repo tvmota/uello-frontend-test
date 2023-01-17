d2f08e8825f5e732f4bb8b9ae28d644bd4d5a7b4
Tiago vieira Mota <tiago.vieira.mota@gmail.com>
2023-01-17 12:36:04 -0300

    Add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+  // import { RouterLink, RouterView } from "vue-router";
+  // import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -17,8 +17,12 @@ import HelloWorld from "./components/HelloWorld.vue";
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">
+          Home
+        </RouterLink>
+        <RouterLink to="/about">
+          About
+        </RouterLink>
       </nav>
     </div>
   </header>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,9 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
+    <button class="btn btn-primary" type="button">
+      Primary button
+    </button>
     <TheWelcome />
   </main>
 </template>
