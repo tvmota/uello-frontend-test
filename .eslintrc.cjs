ca2404ac61770bab7512b98037cefbe7ca544736
Tiago vieira Mota <tiago.vieira.mota@gmail.com>
2023-01-17 12:13:24 -0300

    Config linters

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,26 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier'
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "@vue/eslint-config-airbnb",
   ],
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    "vue/script-indent": ["error", 2, { baseIndent: 1 }],
+    "vue/singleline-html-element-content-newline": ["error", {
+      ignoreWhenNoAttributes: true,
+      ignoreWhenEmpty: true,
+    }],
+  },
+  overrides: [{
+    files: ["*.vue"],
+    rules: { indent: "off" },
+  }],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
